960d5e0310509d9bcf565f3514f9337beb0c2ae4
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-27 18:26:28 +0100

    cambios

--- examen.py
+++ examen.py
@@ -1,7 +1,8 @@
 def filtrar_palabras(lista_palabras, letra):
     lista_final = []
+    letra = letra.lower()
     for i in lista_palabras:
-        if i.startswith() == letra:
+        if i.lower()startswith(letra):
             lista_final.append(i)
-    return lista_final
+    return lista_final.sorted()
 print(filtrar_palabras(["Hola", "adios", "Hielo"], "h"))
